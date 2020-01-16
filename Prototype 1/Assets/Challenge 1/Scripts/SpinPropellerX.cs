f855361c2a434974476366e79bd4fe41fa7e0d56
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-01-16 00:26:41 -0800

    Challenge 1.8 Completed
    The bonus of the challenge to make the propeller spin.

---
+++ Prototype 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public GameObject propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotates the propeller every frame
+        transform.Rotate(0, 0, 50);
+    }
+}
